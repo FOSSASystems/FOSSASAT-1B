a03ab88c29af36b210716084461e7a4760bc70df
Peter Buchegger <peter@pbuchegger.at>
2019-12-16 15:25:40 +0100

    first travis test with cppcheck

---
+++ software/cppcheck.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+set -ex
+
+cppcheck --version
+
+cppcheck --enable=all --suppress=missingIncludeSystem --inconclusive --inline-suppr --error-exitcode=1 \
+    -IFossaSat1B FossaSat1B
+    # --suppress=unusedFunction
+    #--platform=avr8
+
+# docker run -t -i -v $PWD:/src -w /src peterus/fossasat /bin/bash cppcheck.sh
