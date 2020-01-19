4133d4c14dc3b7cb07ce03cd219c35fbab4396dc
Bambofy <rbamfordz@gmail.com>
2020-01-19 13:55:33 +0000

    added documentation to deployment header.

--- software/FossaSat1B/deployment.h
+++ software/FossaSat1B/deployment.h
@@ -3,6 +3,16 @@
 
 #include "FossaSat1B.h"
 
+/**
+ * @file deployment.h
+ * 
+ * @brief The deployment sequence is outlined in the source deployment.cpp.
+ */
+
+/**
+ * @brief This function deploys the antenna by powering the MOSFET1/2 for 1200 seconds.
+ * 
+ */
 void Deployment_Deploy();
 
 #endif
