aace301fa4a755c6c5150bdf0731a6a86c05546d
Peter Buchegger <peterus@users.noreply.github.com>
2020-01-23 23:17:47 +0100

    Update compile.sh

--- software/scripts/compile.sh
+++ software/scripts/compile.sh
@@ -3,5 +3,6 @@ set -ex
 
 #arduino-cli compile -v --fqbn arduino:avr:uno FossaSat1B --build-properties compiler.cpp.extra_flags="-DRADIOLIB_STATIC_ONLY -DFOSSASAT_STATIC_ONLY" --warnings=all
 arduino-cli compile -v --fqbn arduino:avr:uno FossaSat1B --warnings=all
+/root/.arduino15/packages/arduino/tools/avr-gcc/7.3.0-atmel3.6.1-arduino5/bin/avr-nm --size-sort --print-size FossaSat1B/FossaSat1B.arduino.avr.uno.elf
 
 # docker run -t -i -v $PWD:/src -v $PWD/Arduino:/root/Arduino -w /src fossasystems/fossasat /bin/bash scripts/compile.sh
