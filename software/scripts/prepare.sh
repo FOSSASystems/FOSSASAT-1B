1db172bb2e5bb6ac88a027d6c97f0404830edc7e
Peter Buchegger <peter@pbuchegger.at>
2020-01-27 22:00:57 +0100

    build for AtMega328PB working

--- software/scripts/prepare.sh
+++ software/scripts/prepare.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 set -ex
 
-arduino-cli core update-index
+arduino-cli core update-index --additional-urls https://mcudude.github.io/MiniCore/package_MCUdude_MiniCore_index.json
 arduino-cli lib install RadioLib
 git clone https://github.com/FOSSASystems/FOSSA-Comms.git $HOME/Arduino/libraries/FOSSA-Comms
 git clone https://github.com/FOSSASystems/tiny-AES-c.git $HOME/Arduino/libraries/tiny-AES-c
 git clone https://github.com/jarzebski/Arduino-INA226.git $HOME/Arduino/libraries/Arduino-INA226
-git clone https://github.com/rocketscream/Low-Power.git $HOME/Arduino/libraries/Low-Power
+#git clone https://github.com/rocketscream/Low-Power.git $HOME/Arduino/libraries/Low-Power
+git clone https://github.com/canique/Low-Power-Canique.git $HOME/Arduino/libraries/Low-Power
+
 
 # docker run -t -i -v $PWD:/src -v $PWD/Arduino:/root/Arduino -w /src fossasystems/fossasat /bin/bash scripts/prepare.sh
